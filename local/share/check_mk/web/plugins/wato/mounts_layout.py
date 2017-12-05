ec077010e1918540a95e088f988a3ee782549b9a
Sebastian Krieger <s_krieger@web.de>
2017-12-05 20:57:09 +0000

    try to match check_mk_exchange requirements

--- local/share/check_mk/web/plugins/wato/mounts_layout.py
+++ local/share/check_mk/web/plugins/wato/mounts_layout.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python
 # -*- encoding: utf-8; py-indent-offset: 4 -*-
 
+#
+# (c) 2017 Sebastian Krieger <s_krieger@web.de>
+#
+
+# This is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  check_mk is  distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# ails.  You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
 group = "mount_layout"
 subgroup_applications = _("Storage, Filesystems and Files")
 
